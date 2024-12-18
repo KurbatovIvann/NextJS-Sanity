136eab5b4da03a365f844d5b42cc3ad371c52531
Ivan Kurbatov <kurbatov.ivann@gmail.com>
2024-12-18 14:12:30 +0200

    fix lint

--- src/app/(home)/blog/_components/posts-grid.tsx
+++ src/app/(home)/blog/_components/posts-grid.tsx
@@ -1,5 +1,5 @@
 import { PostComponent } from "@/app/(home)/blog/_components/post";
-import { POSTS_QUERYResult } from "@/sanity/types";
+import { POSTS_QUERYResult, POST_QUERYResult } from "@/sanity/types";
 
 type PostsGridProps = {
 	posts: POSTS_QUERYResult;
@@ -8,8 +8,8 @@ type PostsGridProps = {
 export const PostsGrid = ({ posts }: PostsGridProps) => {
 	return (
 		<div className="grid grid-cols-1 items-start gap-x-6 gap-y-10 md:grid-cols-2 xl:grid-cols-3">
-			{posts.map((post) => (
-				<PostComponent post={post} key={post._id} />
+			{posts.map((post: POST_QUERYResult) => (
+				<PostComponent post={post} key={post?._id} />
 			))}
 		</div>
 	);
--- src/app/(home)/blog/page.tsx
+++ src/app/(home)/blog/page.tsx
@@ -4,7 +4,7 @@ import { sanityFetch } from "@/sanity/lib/fetchSenity";
 import { POSTS_SEARCH_QUERY } from "@/sanity/lib/queries/posts";
 import { POSTS_QUERYResult } from "@/sanity/types";
 
-export default async function Blog(props: { searchParams?: Promise }) {
+export default async function Blog(props: { searchParams?: Promise<{ query: string }> }) {
 	const searchParams = await props.searchParams;
 	const searchQuery = searchParams?.query || "";
 
