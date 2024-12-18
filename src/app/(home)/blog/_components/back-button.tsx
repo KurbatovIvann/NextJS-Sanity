46b1728382d604683aef9aaf0f8b57aa37c6e10a
Ivan Kurbatov <kurbatov.ivann@gmail.com>
2024-12-18 15:53:44 +0200

    minor fix

--- src/app/(home)/blog/_components/back-button.tsx
+++ src/app/(home)/blog/_components/back-button.tsx
@@ -7,9 +7,9 @@ export const BackButton = () => {
 	const router = useRouter();
 
 	return (
-		<div className="flex cursor-pointer items-center gap-2" onClick={() => router.back()}>
+		<button className="flex cursor-pointer items-center gap-2" onClick={() => router.back()}>
 			<MoveLeft size={32} />
 			<span className="font-mono text-lg font-bold">Back</span>
-		</div>
+		</button>
 	);
 };
